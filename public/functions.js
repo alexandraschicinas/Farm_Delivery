1bf6f335b6b460cad3cb99f5d0c9eb06b7797d3f
mercea violeta <consilierjuridic.violeta@gmail.com>
2021-04-15 18:29:12 +0300

    show clients from json

--- public/functions.js
+++ public/functions.js
@@ -17,6 +17,7 @@ function showTable(id) {
   let tableToShow = document.getElementById(id);
   tableToShow.style.display = "";
 }
+showTable("client")
 
 function clicks() {
   document.addEventListener("click", (e) => {
@@ -27,3 +28,46 @@ function clicks() {
     }
   });
 }
+clicks();
+
+let allClients = [];
+let editId;
+
+function getHtmlClients(clients){
+    return clients.map(client => {
+        return `<tr>
+        <td>${client.id}</td>
+        <td>${client.name}</td>
+        <td>${client.phone}</td>
+        <td>${client.email}</td>
+        <td>${client.county}</td>
+        <td>${client.city}</td>
+        <td>${client.street}</td>
+         <td>   
+            <a href="#" class="remove-btn" data-id="${client.id}">&#10006;</a>
+            <a href="#" class="edit-btn" data-id="${client.id}">&#9998;</a>
+        </td>
+        </tr>`
+    }).join("")
+    
+}
+function showClients(clients) {
+    const html = getHtmlClients(clients);
+     
+    const tbody = document.querySelector("tbody");
+    tbody.innerHTML = html;
+
+}
+function loadClients(){
+    fetch("http://localhost:3000/clients.json") 
+    .then(r => r.json())
+    .then(clients =>{
+        allClients = clients;
+        showClients(clients);
+    });
+}
+
+loadClients();
+
+ 
+
