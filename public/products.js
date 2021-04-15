3172fa91243332b76c8acbf96d0f338eb6861985
Fineas Berende <bfineas@yahoo.com>
2021-04-15 20:18:32 +0300

    prepared to pared with db

---
+++ public/products.js
@@ -0,0 +1,38 @@
+let allProducts = [];
+let editId1;
+
+function getHtmlProducts(products) {
+    return products.map(product => {
+        return `<tr>
+         <td>${product.id}</td>
+         <td>${product.category}</td>
+         <td>${product.name}</td>
+         <td>${product.quantity}</td>
+         <td>${product.price}</td>
+         <td>${product.date}</td>
+          <td>   
+             <a href="#" class="remove-btn" data-id="${product.id}">&#10006;</a>
+             <a href="#" class="edit-btn" data-id="${product.id}">&#9998;</a>
+         </td>
+         </tr>`
+    }).join("")
+
+}
+function showProducts(products) {
+    const html = getHtmlProducts(products);
+
+    const tbody = document.querySelector("#product tbody");
+    tbody.innerHTML = html;
+
+}
+function loadProducts() {
+    fetch("/products.json")
+        .then(r => r.json())
+        .then(products => {
+            console.warn("nus tiu")
+            allProducts = products;
+            showProducts(products);
+        });
+}
+
+loadProducts();
