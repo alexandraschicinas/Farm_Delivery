d0864f5896d7751a01dba86a09ff229c5be2c8ed
Adelinanica <adelinanica29@yahoo.com>
2021-04-15 19:46:11 +0300

    read products from db

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
 var clientsRouter = require('./routes/clients');
+var productsRouter = require('./routes/products');
 
 var app = express();
 
@@ -24,6 +25,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/clients', clientsRouter);
+app.use('/products', productsRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
