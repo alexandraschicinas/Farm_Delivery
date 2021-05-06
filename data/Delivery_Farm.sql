7485477ecd37e7a1e3bbecf82fc4ff7def811d7e
Fineas Berende <bfineas@yahoo.com>
2021-05-06 20:29:27 +0300

    create db invoice and invoice product

--- data/Delivery_Farm.sql
+++ data/Delivery_Farm.sql
@@ -2,10 +2,10 @@
 -- version 5.0.4
 -- https://www.phpmyadmin.net/
 --
--- Gazdă: localhost
--- Timp de generare: apr. 15, 2021 la 05:29 PM
--- Versiune server: 10.4.17-MariaDB
--- Versiune PHP: 7.3.27
+-- Host: localhost
+-- Generation Time: May 06, 2021 at 07:27 PM
+-- Server version: 10.4.17-MariaDB
+-- PHP Version: 7.3.27
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -18,13 +18,13 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8mb4 */;
 
 --
--- Bază de date: `Delivery_Farm`
+-- Database: `Delivery_Farm`
 --
 
 -- --------------------------------------------------------
 
 --
--- Structură tabel pentru tabel `category`
+-- Table structure for table `category`
 --
 
 CREATE TABLE `category` (
@@ -33,7 +33,7 @@ CREATE TABLE `category` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 --
--- Eliminarea datelor din tabel `category`
+-- Dumping data for table `category`
 --
 
 INSERT INTO `category` (`id`, `name`) VALUES
@@ -43,7 +43,7 @@ INSERT INTO `category` (`id`, `name`) VALUES
 -- --------------------------------------------------------
 
 --
--- Structură tabel pentru tabel `clients`
+-- Table structure for table `clients`
 --
 
 CREATE TABLE `clients` (
@@ -57,17 +57,50 @@ CREATE TABLE `clients` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 --
--- Eliminarea datelor din tabel `clients`
+-- Dumping data for table `clients`
 --
 
 INSERT INTO `clients` (`id`, `name`, `phone`, `email`, `county`, `city`, `street`) VALUES
-(1, 'Fineas ', 754690859, 'bfineas@yahoo.com', 'Cluj', 'Floresti', 'prof. i. rus, nr. 56'),
-(2, 'Ciprian Chesa', 755323228, 'chesa_ciprian@yahoo.com', 'Cluj', 'Cluj-Napoca', 'Gr. Alexandrescu');
+(1, 'Fineas', 754690859, 'bfineas@yahoo.com', 'Cluj', 'Floresti', 'prof. i. rus, nr. 56'),
+(5, 'wrefdrfg', 2321, 'dscs@yahoo.com', 'sd', 'sd', 'sd');
 
 -- --------------------------------------------------------
 
 --
--- Structură tabel pentru tabel `products`
+-- Table structure for table `invoiceProducts`
+--
+
+CREATE TABLE `invoiceProducts` (
+  `ID` int(11) NOT NULL,
+  `invoiceId` int(11) NOT NULL,
+  `product` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `Invoices`
+--
+
+CREATE TABLE `Invoices` (
+  `ID` int(11) NOT NULL,
+  `clientId` int(11) NOT NULL,
+  `delivery` date NOT NULL,
+  `date` date NOT NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Dumping data for table `Invoices`
+--
+
+INSERT INTO `Invoices` (`ID`, `clientId`, `delivery`, `date`) VALUES
+(1, 1, '2021-05-12', '2021-05-06'),
+(2, 1, '2021-05-14', '2021-05-06');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `products`
 --
 
 CREATE TABLE `products` (
@@ -80,56 +113,101 @@ CREATE TABLE `products` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 --
--- Eliminarea datelor din tabel `products`
+-- Dumping data for table `products`
 --
 
 INSERT INTO `products` (`id`, `category`, `name`, `quantity`, `price`, `date`) VALUES
 (1, 1, 'morcovi', 100, 2, '2021-04-15'),
-(2, 2, 'cirese', 100, 2, '2021-04-15');
+(2, 2, 'cirese', 200, 2022, '2021-04-15'),
+(3, 1, 'dd', 21, 2, '2021-04-15');
 
 --
--- Indexuri pentru tabele eliminate
+-- Indexes for dumped tables
 --
 
 --
--- Indexuri pentru tabele `category`
+-- Indexes for table `category`
 --
 ALTER TABLE `category`
   ADD PRIMARY KEY (`id`);
 
 --
--- Indexuri pentru tabele `clients`
+-- Indexes for table `clients`
 --
 ALTER TABLE `clients`
   ADD PRIMARY KEY (`id`);
 
 --
--- Indexuri pentru tabele `products`
+-- Indexes for table `invoiceProducts`
+--
+ALTER TABLE `invoiceProducts`
+  ADD PRIMARY KEY (`ID`),
+  ADD KEY `invoiceId` (`invoiceId`),
+  ADD KEY `productId` (`product`);
+
+--
+-- Indexes for table `Invoices`
+--
+ALTER TABLE `Invoices`
+  ADD PRIMARY KEY (`ID`),
+  ADD KEY `clientId` (`clientId`);
+
+--
+-- Indexes for table `products`
 --
 ALTER TABLE `products`
   ADD PRIMARY KEY (`id`);
 
 --
--- AUTO_INCREMENT pentru tabele eliminate
+-- AUTO_INCREMENT for dumped tables
 --
 
 --
--- AUTO_INCREMENT pentru tabele `category`
+-- AUTO_INCREMENT for table `category`
 --
 ALTER TABLE `category`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 
 --
--- AUTO_INCREMENT pentru tabele `clients`
+-- AUTO_INCREMENT for table `clients`
 --
 ALTER TABLE `clients`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
 
 --
--- AUTO_INCREMENT pentru tabele `products`
+-- AUTO_INCREMENT for table `invoiceProducts`
+--
+ALTER TABLE `invoiceProducts`
+  MODIFY `ID` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `Invoices`
+--
+ALTER TABLE `Invoices`
+  MODIFY `ID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+
+--
+-- AUTO_INCREMENT for table `products`
 --
 ALTER TABLE `products`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `invoiceProducts`
+--
+ALTER TABLE `invoiceProducts`
+  ADD CONSTRAINT `invoiceId` FOREIGN KEY (`invoiceId`) REFERENCES `Invoices` (`ID`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `productId` FOREIGN KEY (`product`) REFERENCES `products` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Constraints for table `Invoices`
+--
+ALTER TABLE `Invoices`
+  ADD CONSTRAINT `clientId` FOREIGN KEY (`clientId`) REFERENCES `clients` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
